364c562f6b08b83dc5f5cad78dddadbb9ac26150
giyeol Suk <giyeol Suk>
2016-11-27 18:35:52 +0900

    빌드 설정 및 좌표 값 받는 부분까지만

--- KS_android/app/src/main/java/com/untoc/ks_android/MainActivity.java
+++ KS_android/app/src/main/java/com/untoc/ks_android/MainActivity.java
@@ -1,14 +1,62 @@
 package com.untoc.ks_android;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView logView;
+    Boolean isGPSEnabled, isNetworkEnabled;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //
+
+        Log.d("Main", "onCreate");
+
+        logView = (TextView) findViewById(R.id.log);
+        logView.setText("GPS 가 잡혀야 좌표가 구해짐");
+
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        // GPS 프로바이더 사용가능여부
+        isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        // 네트워크 프로바이더 사용가능여부
+        isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        Log.d("Main", "isGPSEnabled=" + isGPSEnabled);
+        Log.d("Main", "isNetworkEnabled=" + isNetworkEnabled);
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                double lat = location.getLatitude();
+                double lng = location.getLongitude();
+
+                logView.setText("latitude: " + lat + ", \nlongitude: " + lng);
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+                logView.setText("onStatusChanged");
+            }
+
+            public void onProviderEnabled(String provider) {
+                logView.setText("onProviderEnabled");
+            }
+
+            public void onProviderDisabled(String provider) {
+//                logView.setText("onProviderDisabled");
+            }
+        };
+
+        // Register the listener with the Location Manager to receive location updates
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
     }
 }
